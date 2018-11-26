aeff4227494e7a268df0a3ed9d32d0249bee813e
ricfield <ricfield@gmail.com>
2018-11-26 09:38:52 -0500

    Update npi_lt.py

--- npi_lt.py
+++ npi_lt.py
@@ -3,28 +3,33 @@ import urllib.request, json
 input_valid = True
 run = True
 
-while run == True:
+def main():
+   while run == True:
 
    
-   postal_code = input("Enter Postal Code (Press enter for none): ")
-   last_name = input("Enter Last Name (Press enter for none): ")
-   first_name = input("Enter First Name  (Press enter for none): ")
+      postal_code = input("Enter Postal Code (Press enter for none): ")
+      last_name = input("Enter Last Name (Press enter for none): ")
+      first_name = input("Enter First Name  (Press enter for none): ")
 
+      pv information_json = getNPIInformationJSON(postal_code, last_name, first_name)
+      
+      print("\n\nResults:\n")
+
+      print("NPI          Last Name                      First Name                   ")
+      print("-------------------------------------------------------------------------")
+      
+      
+      for pv in pv_information_json['results']:
+         print('{:10}'.format(str(pv['number'])) + "   " + '{:31}'.format(pv['basic']['last_name']) + '{:30}'.format(pv['basic']['first_name']))
+
+      print("\n")
+
+def getNPIInformationJSON(ZIP, lastName, firstName):
    nppes_url = urllib.request.urlopen("https://npiregistry.cms.hhs.gov/api/?limit=200&number=&enumeration_type=1&taxonomy_description=&last_name=" + last_name + "&firstname=" + first_name + "&postal_code=" + postal_code)
-
-   pv_information_json = json.loads(nppes_url.read())
-
-
+   providerJSONData = json.loads(nppes_url.read())
+   return providerJSONData
    
-   print("\n\nResults:\n")
-
-   print("NPI          Last Name                      First Name                   ")
-   print("-------------------------------------------------------------------------")
-      
-      
-   for pv in pv_information_json['results']:
-      print('{:10}'.format(str(pv['number'])) + "   " + '{:31}'.format(pv['basic']['last_name']) + '{:30}'.format(pv['basic']['first_name']))
-
-
-   print("\n")
-
+   
+if __name__ == '__main__':
+   main()
+   
